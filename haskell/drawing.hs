29b345e456e473483dac5ee562811ceb4e51d8e9
Eva <e.n.timmer@students.uu.nl>
2020-03-17 12:57:54 +0100

    verbeterd

--- haskell/drawing.hs
+++ haskell/drawing.hs
@@ -7,7 +7,7 @@ width = 1000
 
 height = 1000
 
-boxSize = 15
+boxSize = 1
 
 boxPath = [(0,0),(boxSize,0),(boxSize,boxSize),(0,boxSize)]
 
@@ -25,7 +25,7 @@ main
 	picture			 -- picture to display
 
 picture
-    = Pictures (concat [(leader (100,100) [1,2,3] 100 45 1),(leader (100,300) [1] 100 0 0), (leader (100,500) [1] 100 (-45) 1)])
+    = Pictures (concat [(leader (100,1) [1] 2 (-45) 1),(leader (100,3) [1] 2 0 0), (leader (100,5) [1] 2 (45) 1)])
 
     -- array ((1,1),(4,4)) [((1,1),True),((1,2),False),((1,3),False),((1,4),False),((2,1),True),((2,2),False),((2,3),False),((2,4),False),((3,1),True),((3,2),False),((3,3),False),((3,4),False),((4,1),True),((4,2),False),((4,3),False),((4,4),False)]
 
@@ -40,8 +40,11 @@ _label numbers port position x
 boxr rotation position i
     = Pictures [Rotate rotation $ lineLoop boxPath,Rotate rotation $ Translate 2 2 $  Scale 0.1 0.1 $ Text (show i)]
 
---Unrotated box
-box o position i = Pictures [Translate position ((fromIntegral o)*boxSize*(-1)) $ lineLoop boxPath,Translate 2 (2+((fromIntegral o)*boxSize*(-1))) $  Scale 0.1 0.1 $ Text (show i)]
+--Unrotated box (no clues)
+box o position = Pictures [Translate (position*boxSize) ((fromIntegral o)*boxSize*(-1)) $ lineLoop boxPath]
+
+--Rotated clue
+clue o m position i = Pictures [Translate (7+(position*boxSize)) (7+((fromIntegral o)*boxSize*(-1))) $ Rotate m $ Translate (-5) (-5) $ Scale 0.1 0.1 $ Text (show i)]
 
 offset position path
 	= map (plus position) path
@@ -55,6 +58,11 @@ angle (a,b) (c,d) = -(atan ((d-b)/(c-a))) * (180/pi)
 
 -- Functions required
 -- Draw line (leader) from starting point (port) length (extent) angle and numbers
-leader :: (Float,Float) -> [Int] -> Float -> Float -> Int -> [Picture]
-leader p@(x,y) numbers len m o = map (Translate x y) ((Rotate m $ Line [(0,0),(len,0)]): map (Rotate m) ( map (Translate len 0) (zipWith (box o) [0..fromIntegral(length numbers)*boxSize] numbers)))
-
+-- The text needs to not rotate
+leader :: (Float,Float) -- The position of the port
+	-> [Int] 			-- The list of numbers(clues) that need to be shown
+	-> Float 			-- The length of the leader
+	-> Float 			-- The slope of the leader
+	-> Int 				-- The side of the line 1: bottom and 0: top
+	-> [Picture]
+leader p@(x,y) numbers len m o = map (Translate x y) ((Rotate m $ Line [(0,0),(len,0)]): map (Rotate m) ( map (Translate len 0) ((map (box o) [0..fromIntegral((length numbers)-1)]) ++ zipWith (clue o (-m)) [0..fromIntegral(length numbers)] numbers)))
--- haskell/labeling.hs
+++ haskell/labeling.hs
@@ -15,42 +15,45 @@ data Line = Line Port Port Integer Integer
 data Label = Label {l::Integer}
 type Labeling = [(Label,Port,Integer)] --Label,Port and extension length
 
-p1 = Port Labeling.Right 1 0.5
+p1 = Port Labeling.Right 1 (-0.1)
 p2 = Port Labeling.Right 3 1
-p3 = Port Labeling.Right 5 (-0.5)
+p3 = Port Labeling.Right 5 (0.1)
 ps = listArray (1,3) [p1,p2,p3]
 
+test1 = labelable (1,3) (3,1) ps
+
 -- (port number, extent) (port number, extent) list of ports
-labelable :: (Integer,Integer)->(Integer,Integer)->Array Integer Port->IO()
-labelable (p1,e1) (p2,e2) ps = 
-    do 
-        let result = r!(p1,p2)
-        print result
+labelable :: (Integer,Integer)->(Integer,Integer)->Array Integer Port->Array ((Integer,Integer),(Integer,Integer)) Bool
+labelable (p1,e1) (p2,e2) ps = r!(p1,p2)
             where
                 r = array ((p1,p1),(p2,p2)) [((i,j),(f (i,j)))|i<-[p1..p2],j<-[p1..p2]]
                 eMax = e1 + e2
-                f (i,j) = listArray ((1,1),(eMax,eMax)) (map g [(a,b,c,d)|a<-[i],b<-[j],c<-[1..eMax],d<-[1..eMax]])
+                f (i,j) = array (((i,i),(1,1)),((j,j),(eMax,eMax)))  (map g [(a,b,c,d)|a<-[i..j],b<-[i..j],c<-[1..eMax],d<-[1..eMax]]) -- for each pair of ports, a list of possible lengths and validity
                     where
-                        g (i,j,ei,ej)
-                            | i == j = True
-                            | i > j = False
-                            | j-i == 1 = not (leaderIntersect posi mi posj mj (ei,ej))
-                            | otherwise = or (map (h i j l ei ej) [1..eMax]) 
+                        g (i,j,ei,ej) -- if length ei for port i and length ej for port j are a valid pair
+                            | i == j = (((i,j),(ei,ej)),True)
+                            | i > j = (((i,j),(ei,ej)),False)
+                            | j-i == 1 = (((i,j),(ei,ej)),k) -- if they are neighbouring ports
+                            | otherwise = (((i,j),(ei,ej)),m)
                             where
-                                l =  floor((fromIntegral(i+j))/2)
+                                l =  floor((fromIntegral(i+j))/2) -- port in the middle of i and j
                                 Port _ posi mi = ps!i
                                 Port _ posj mj = ps!j
                                 Port _ posl ml = ps!l
-                                h i j l ei ej el = r!(i,l)!(ei,el) && r!(l,j)!(el,ej) && (fitLength (ps!i) ei (ps!j) ej (ps!l) el)
+                                h i j l ei ej el = r!(i,l)!((i,l),(ei,el)) && r!(l,j)!((l,j),(el,ej)) && (fitLength (ps!i) ei (ps!j) ej (ps!l) el)
+                                k = not (leaderIntersect posi mi posj mj (ei,ej))
+                                m = or (map (h i j l ei ej) [1..eMax])
                                         
 
 --otherwise = and (concat (zipWith map (cycle (map (fitLength (ps!p1) e1 (ps!p2) e2) (map (ps!) [(p1+1)..(p2-1)]))) (permutations [1..(e1+e2)])))
 
+-- shorthand for fitlength, index of first port, index of second port, index of fitting port, length first port, length second port, length fitting port, all ports
 h i j k ei ej ek ps = (fitLength (ps!i) ei (ps!j) ej (ps!k) ek)
 
 findFits :: Port -> Integer -> Port -> Integer -> Port -> [Integer]
 findFits p1 len1 p2 len2 p = filter (fitLength p1 len1 p2 len2 p) [1..len1+len2]
 
+-- determines if a leader with length len fits between two ports with lengths len1 and len2
 fitLength :: Port -> Integer -> Port -> Integer -> Port -> Integer -> Bool
 fitLength (Port s1 pos1 m1) len1 (Port s2 pos2 m2) len2 (Port s pos m) len 
     | s1 /= s2 = True
